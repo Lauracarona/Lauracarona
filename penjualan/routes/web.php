648a4fe8d05db95ae3ade65477b04aff21ca8081
Lauracarona <lauracarona2016@gmail.com>
2019-03-22 09:28:32 +0700

    publish file index

--- penjualan/routes/web.php
+++ penjualan/routes/web.php
@@ -10,7 +10,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/welcome', function () {
+echo "Welcome";
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/index', function () {
+echo "Uji Coba route dengan method GET";
+});
+
+Route::post('/store', function () {
+// sintak untuk menyimpan data
+});
+
+Route::put('/update', function () {
+// sintak untuk upadte data
+});
+
+Route::get('/produk', 'produkController@index');
 });
